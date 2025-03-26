7359a8a0f951acfe21066e398e4c66f4d9a351bb
YasaraD <127465894+YasaraD@users.noreply.github.com>
2025-03-27 01:22:57 +0530

    deleted unwanted files

--- employee-management/employee-frontend/src/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-// Create an Axios instance with base URL pointing to the Laravel API
-const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api/", // URL of the Laravel backend
-});
-
-export default axiosInstance;
